6f51ae75a6027050b60d013404e246a0439d7aba
burakmey <burak_meydan-gs@hotmail.com>
2024-05-01 17:13:14 +0300

    Routing added.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import GlobalStyle from "./constants/GlobalStyle";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <GlobalStyle />
+    <Routes>
+      <Route path="/*" element={<App />} />
+    </Routes>
+  </BrowserRouter>
+);
