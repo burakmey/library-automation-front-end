ad9f3fd24d06a3d44bd57b0dc441212a54b6bc50
burakmey <burak_meydan-gs@hotmail.com>
2024-05-01 17:13:49 +0300

    Routes added to App.js file.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import { useEffect } from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
+import Login from "./pages/Login";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  console.log("App mounted!");
+
+  useEffect(() => {
+    return () => console.log("App unmounted!");
+  }, []);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* Public Routes */}
+        <Route path="/" element={<Login />} />
+        {/* Protected Routes */}
+
+        {/* Catch All */}
+        <Route
+          path="*"
+          element={
+            <div>
+              404 Not Found back to home button <Link to="/">Home</Link>
+            </div>
+          }
+        />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
