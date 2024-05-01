1f87c61142f17be856f84736fe16633c7e8e1dc8
burakmey <burak_meydan-gs@hotmail.com>
2024-05-01 17:15:50 +0300

    GlobalStyle added.

---
+++ src/constants/GlobalStyle.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+:root{
+    /* COLORS */
+    --header-primary: #f2f3f5;
+    --font-primary: #b5bac1;
+    --text-muted: #949ba4;
+    --text-normal: #dbe1de;
+    --input-text: #dbdee1;
+    --brand-experiment: #5c64f4;
+    --text-link: #00a8fc;
+    --background-primary: #313338;
+    --background-primary-blue: #5865f2;
+    --background-modifier-accent: #4e50587a;
+    --input-background: #1e1f22;
+}`;
+
+export default GlobalStyle;
