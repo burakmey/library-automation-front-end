0878e021d349d47102fa31f47b53156f14f79ec9
burakmey <burak_meydan-gs@hotmail.com>
2024-05-03 22:57:44 +0300

    Pages added.

---
+++ src/pages/Pages.js
@@ -0,0 +1,8 @@
+import Home from "./Home";
+import Login from "./Login";
+import Register from "./Register";
+import SearchBook from "./SearchBook";
+import AdminPanel from "./AdminPanel";
+import AddBook from "./AddBook";
+
+export { Home, Login, Register, SearchBook, AdminPanel, AddBook };
