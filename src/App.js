9471799da8b522497e672dfaf17ac8e4d2f13891
burakmey <burak_meydan-gs@hotmail.com>
2024-05-03 22:43:15 +0300

    New routes added.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import { useEffect } from "react";
 import { Link, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+import { AdminPanel, SearchBook, Home, Login, Register, AddBook } from "./pages/Pages";
 
 function App() {
   console.log("App mounted!");
@@ -15,8 +14,14 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         {/* Public Routes */}
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/search" element={<SearchBook />} />
+        <Route path="/admin" element={<Layout />}>
+          <Route path="/admin" element={<AdminPanel />} />
+          <Route path="/admin/add" element={<AddBook />} />
+        </Route>
         {/* Protected Routes */}
 
         {/* Catch All */}
