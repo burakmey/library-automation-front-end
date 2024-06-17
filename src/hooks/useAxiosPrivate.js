9731aedac1ad4895300ce5a2f4a95270553ff042
burakmey <burak_meydan-gs@hotmail.com>
2024-06-17 15:14:01 +0300

    Hooks added.

---
+++ src/hooks/useAxiosPrivate.js
@@ -0,0 +1,46 @@
+import { axiosPrivate } from "../api/axios";
+import { useEffect } from "react";
+import useRefreshToken from "./useRefreshToken";
+import useAuthContext from "./useAuthContext";
+
+const useAxiosPrivate = () => {
+  console.log("useAxiosPrivate mounted!");
+  const refresh = useRefreshToken();
+  const { userData } = useAuthContext();
+
+  useEffect(() => {
+    const requestIntercept = axiosPrivate.interceptors.request.use(
+      (request) => {
+        if (request.headers["Authorization"] !== userData.accessToken) {
+          request.headers["Authorization"] = `Bearer ${userData.accessToken}`;
+        }
+        return request;
+      },
+      (error) => {
+        return Promise.reject(error);
+      }
+    );
+    const responseIntercept = axiosPrivate.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      async (error) => {
+        const previousRequest = error?.config;
+        await refresh()
+          .then(() => {
+            previousRequest.headers["Authorization"] = `Bearer ${userData.accessToken}`;
+            return axiosPrivate(previousRequest);
+          })
+          .catch((error) => Promise.reject(error));
+      }
+    );
+    return () => {
+      axiosPrivate.interceptors.request.eject(requestIntercept);
+      axiosPrivate.interceptors.response.eject(responseIntercept);
+      console.log("useAxiosPrivate unmounted!");
+    };
+  }, [accessToken, refresh]);
+  return axiosPrivate;
+};
+
+export default useAxiosPrivate;
