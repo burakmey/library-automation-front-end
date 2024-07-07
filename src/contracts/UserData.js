918a973e37d09a184b4514aa3a34cfc4496a536b
burakmey <burak_meydan-gs@hotmail.com>
2024-07-08 00:26:30 +0300

    UserData contract updated.

--- src/contracts/UserData.js
+++ src/contracts/UserData.js
@@ -1,5 +1,5 @@
 export class UserData {
-  constructor({ accessToken, email, name, surname, registeredAt, nation, role }) {
+  constructor({ accessToken, email, name, surname, registeredAt, nation, role, fines, userBookBorrows, userBookReserves, desires }) {
     this.accessToken = accessToken;
     this.email = email;
     this.name = name;
@@ -7,5 +7,9 @@ export class UserData {
     this.registeredAt = registeredAt;
     this.nation = nation;
     this.role = role;
+    this.fines = fines;
+    this.userBookBorrows = userBookBorrows;
+    this.userBookReserves = userBookReserves;
+    this.desires = desires;
   }
 }
