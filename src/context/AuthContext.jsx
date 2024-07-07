a46ef46451b0d69d986b88c59e11a88d4df0dc12
burakmey <burak_meydan-gs@hotmail.com>
2024-07-08 00:25:01 +0300

    Desire, Reservation, Borrow datas added to userData.

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -10,11 +10,27 @@ export const AuthProvider = ({ children }) => {
   const updateUserData = (response) => {
     const user = new UserData(response);
     setUserData(user);
+    //console.log(user);
   };
-  const values = { userData, updateUserData };
+  const updateUserDesires = (response) => {
+    const user = new UserData(userData);
+    user.desires = response;
+    setUserData(user);
+  };
+  const updateUserReservations = (response) => {
+    const user = new UserData(userData);
+    user.userBookReserves = response;
+    setUserData(user);
+  };
+  const updateUserBorrows = (response) => {
+    const user = new UserData(userData);
+    user.userBookBorrows = response;
+    setUserData(user);
+  };
+  const values = { userData, updateUserData, updateUserDesires, updateUserReservations, updateUserBorrows };
 
   useEffect(() => {
-    return () => console.log("App unmounted!");
+    return () => console.log("AuthContext unmounted!");
   }, []);
 
   return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
