df3b74d7535f9870927ee215d0c89d546e13cfcc
burakmey <burak_meydan-gs@hotmail.com>
2024-07-19 21:19:11 +0300

    Situation enums added.

---
+++ src/constants/SituationEnums.js
@@ -0,0 +1,17 @@
+export const borrowSituationEnum = {
+  borrowed: "Borrowed",
+  returned: "Returned",
+  timeOut: "TimeOut",
+};
+
+export const desireSituationEnum = {
+  borrow: "Borrow",
+  reserveBorrow: "ReserveBorrow",
+  return: "Return",
+};
+
+export const reserveSituationEnum = {
+  waiting: "Waiting",
+  borrowed: "Borrowed",
+  timeOut: "TimeOut",
+};
